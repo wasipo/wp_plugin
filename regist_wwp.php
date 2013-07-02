8498f06991e35d63876078a4c913ac832ce29e46
horiguchi <horiguchiko@gmail.com>
2013-07-02 14:40:00 +0900

    henkou

--- regist_wwp.php
+++ regist_wwp.php
@@ -1,1 +1,25 @@
-abcd
+<?php
+/*
+Plugin name: wp_MeiboMan
+Plugin URI: http://mikaduki.info/
+Description: meibo
+Version: 0.0.1
+Author: kouhei
+Author URI: http://mikaduki.info/
+License : GPL
+*/
+
+require_once("wp_meibo_loader.php");
+$classLoader = new ClassLoader();
+//var_dump(dirname(__FILE__));
+$classLoader->registerDir(dirname(__FILE__));
+//$showtext = new wp_meibo_setting;
+
+
+
+
+
+
+
+
+?>
