b9e909f5603f1aee4c35f168e712df9d9ff3a1f7
horiguchi <horiguchiko@gmail.com>
2013-07-03 11:00:37 +0900

    →メソッドを共用に変更した。 →数字とかのエラーチェックが必要になる。 →DBの設定をどうするか。 →表ページへの表示の仕方がわからんちん。

--- wp_meibo_setting.php
+++ wp_meibo_setting.php
@@ -3,6 +3,9 @@
 class wp_meibo_setting 
 {
 
+    private $meibo_data;
+
+
     function __construct() {
       add_action('admin_menu', array($this, 'wp_meibo_add_pages'));
     }
@@ -17,6 +20,18 @@ class wp_meibo_setting
 
     public function wp_meibo_add_html_pages() 
     {
+
+      $this->meibo_data = $_POST;
+      
+      if($this->wp_meibo_postdata())
+      {
+        $_SESSION["meibo_post_data"] = $_POST;
+        $this->meibo_data = $_SESSION["meibo_post_data"];
+        $this->wp_meibo_postdata();
+      }
+
+
+
         echo <<<EOF
 
 
@@ -27,19 +42,40 @@ class wp_meibo_setting
             });
         </script>
 
-        <form action="" method="post">
-              <input type="radio" /><Br />
-              <input type="radio" /><Br />
-              <input type="radio" /><Br />
-              <input type="radio" /><Br />
+        <form action="#" method="post">
+              <br />
+              <br />
+              <br />
+              何個項目作る？<input type="text" name="meibo_colum" />
+              <br />
+              DBどうする？
+              &nbsp;&nbsp;&nbsp;使わない<input type="radio" name="meibo_db_use" checked="checked" value="1" />
+              使いたい<input type="radio" name="meibo_db_use" value="2" />
               <input type="submit" value="送信" />
         </form>
-
-
 EOF;
+        
+
 
 
     }
+
+    //このメソッドはセッションでもPOSTでも使う
+    public function wp_meibo_postdata()
+    {
+        foreach($this->meibo_data as $val)
+        {
+
+            if(!empty($val))
+            {
+                return true;
+            }
+
+        }
+
+    }
+
+
 
 }
 
