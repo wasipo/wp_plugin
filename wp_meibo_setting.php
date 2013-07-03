9f6d580d228327f32011a51e75cab1704a1f8a8a
horiguchi <horiguchiko@gmail.com>
2013-07-03 10:10:10 +0900

    ファイル追加、DB用のクラス

--- wp_meibo_setting.php
+++ wp_meibo_setting.php
@@ -18,6 +18,8 @@ class wp_meibo_setting
     public function wp_meibo_add_html_pages() 
     {
         echo <<<EOF
+
+
         <script>
             jQuery(function()
             {
@@ -25,14 +27,13 @@ class wp_meibo_setting
             });
         </script>
 
-
-
-            <form action="" method="post">
-                <input type="text" /><Br />
-                <input type="text" /><Br />
-                <input type="text" /><Br />
-                <input type="text" /><Br />
-            </form>
+        <form action="" method="post">
+              <input type="radio" /><Br />
+              <input type="radio" /><Br />
+              <input type="radio" /><Br />
+              <input type="radio" /><Br />
+              <input type="submit" value="送信" />
+        </form>
 
 
 EOF;
@@ -42,37 +43,3 @@ EOF;
 
 }
 
-class wp_meibo_regit_database
-{
-
-
-    function cmt_activate() {
-        global $wpdb;
-        //DBのバージョン
-        $cmt_db_version = '1.0';
-        //現在のDBバージョン取得
-        $installed_ver = get_option( 'cmt_meta_version' );
-            // DBバージョンが違ったら作成
-            if( $installed_ver != $cmt_db_version ) 
-            {
-                $sql = "CREATE TABLE " . $this->in_form_db . " (
-                  meta_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                  post_id bigint(20) UNSIGNED DEFAULT '0' NOT NULL,
-                  item_name text,
-                  item_name1 text,
-                  item_name2 text,
-                  item_name3 text,
-                  item_name4 text,
-                  item_name5 text,
-                  UNIQUE KEY meta_id (meta_id)
-                )
-                CHARACTER SET 'utf8';";
-                require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-                dbDelta($sql);
-                //オプションにDBバージョン保存
-                update_option('cmt_meta_version', $cmt_db_version);
-            }
-    }
-
-
-}
